e17f64189174e00130c7cd63f464ccfbf8159246
pkamra97 <109149360+pkamra97@users.noreply.github.com>
2022-09-02 22:00:29 +0530

    feat : validations

--- app.js
+++ app.js
@@ -7,33 +7,47 @@ console.log(inputs);
 console.log(inputs[0]);
 function calculateSum(angle1,angle2,angle3)
 {
-const sum=angle1+angle2+angle3; 
-console.log(sum);
-return sum;
+ if(angle1 > 0 && angle2 > 0 && angle3 > 0 )
+  {
+     const sum=angle1+angle2+angle3; 
+     console.log(sum);
+     return sum;
+  }
+  else{
+    console.log("Please enter values > zero(0)");
+    outputE1.innerText="Please enter values > zero(0)";
+  }
 }
 // doubt 2 console not printing sum value but in video it is working
 
 function isTriangle(){
 // const total=calculateSum(45,45,90);
 // console.log(inputs[0].value,inputs[1].value,inputs[2].value);
+if(Number(inputs[0].value) && Number(inputs[1].value) && Number(inputs[2].value)){
 const total=calculateSum(Number(inputs[0].value) , Number(inputs[1].value) , Number(inputs[2].value));
 // console.log(total);
 // also not working in console.
 
-if(total===180)
-{
+   if(total===180)
+   {
     console.log("The angles form a triangle");
     // this is not visible in console 
 
     // but this is working in main html page
     outputE1.innerText="The angles form a triangle";
-}
-else{
+   }
+   else{ 
     console.log("The angles don't form a triangle");
     // this is not visible in console 
 
     // but this is working in main html page
     outputE1.innerText="The angles don't form a triangle";
+   }
+}
+else{
+
+    outputE1.innerText="Please fill all values";
+
 }
 
 }
